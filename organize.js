824bddbb4178f48d97d874ad6212bdb0d0ddfd33
Manishsharma1208 <brahmanmanish1208@gmail.com>
2022-07-05 00:29:58 +0530

    create and read file in download folder

--- organize.js
+++ organize.js
@@ -10,6 +10,7 @@ let types = {
 }
 
 function organize(srcpath){
+    //1. check the srcpath is present or not
     if(srcpath == undefined){
         //console.log(srcpath); //undefined return
         srcpath =process.cwd();//the process.cwd()method return the current working directory of node.js process
@@ -17,6 +18,7 @@ function organize(srcpath){
         srcpath);
 
     }
+    //2. create the directory - Organized_files
     let organisedfiles = path.join(srcpath,"Organised_files");
     //console.log("Organized files folder path is",organisedfiles);
     if(fs.existsSync(organisedfiles)==false){//organizedfiles naam ka folder nhi h to bna do warna rehne do
@@ -25,6 +27,17 @@ function organize(srcpath){
     else{
         console.log("folder already exist");
     }
-    
+    //4. read all the content of the directory-basically read the name of files present in directory
+    let allfiles =fs.readdirSync(srcpath);
+    console.log(allfiles);
+
+    //5. traverse over all the files and classify on the basis of extention(.pdf,.mp3,.mp4,.exe)
+    for(let i=0;i<allfiles.length;i++){
+        let ext =allfiles[i].split(".")[1];
+        console.log(ext);
+    }
 }
-organize();
+
+//3. create Organised_files folder in downloads folder
+let srcpath ="C:\\Users\\Manish\\OneDrive\\Desktop\\hello world\\node\\fileOrganizer\\downloads"
+organize(srcpath);
